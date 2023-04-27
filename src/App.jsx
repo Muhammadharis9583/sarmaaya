2360ae6a0a0625a68ebcbc462bae152509d21172
haris <m.haris8790@gmail.com>
2023-04-27 12:37:37 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 style={{color:"darkgreen"}}>Sarmaaya.pk</h1>
+        <img
+          src="https://scontent.fisb1-2.fna.fbcdn.net/v/t1.6435-9/107169168_157506672535703_4313405693061431182_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=DoiaJfRlc6cAX-IWKSo&_nc_ht=scontent.fisb1-2.fna&oh=00_AfAcjULtIv5Luf7dlvwWyB6UjM9yYw7kcgxGghzNDdF6HA&oe=64719E63"
+          height={500}
+          width={500}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App
