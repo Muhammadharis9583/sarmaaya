60b4af23d6ad6c912ab9cfc6b19697d5b4db4162
hammadkh0 <hammad5718@gmail.com>
2023-05-09 10:10:49 +0500

    Fix: Removed conflicting classes from 404 page with module.css

--- src/components/utils/Page404.jsx
+++ src/components/utils/Page404.jsx
@@ -1,13 +1,14 @@
-import "./404.css";
+import { NavLink } from "react-router-dom";
+import styles from "./404.module.css";
 
 const Page404 = () => {
   return (
-    <div className="container bg-white">
-      <div className="title">404</div>
-      <div className="message">Sorry, the page you requested could not be found.</div>
-      <a className="link" href="/">
+    <div className={styles.container}>
+      <div className={styles.title}>404</div>
+      <div className={styles.message}>Sorry, the page you requested could not be found.</div>
+      <NavLink className={styles.link} to="/symbols">
         Go back to homepage
-      </a>
+      </NavLink>
     </div>
   );
 };
